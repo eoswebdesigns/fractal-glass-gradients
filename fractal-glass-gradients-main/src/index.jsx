cc8448b4408e7afee74bc663c977e7550227bf08
eoswebdesigns <eoswebdesigns@gmail.com>
2026-09-06 22:06:31 +0200

    Update index.jsx

--- fractal-glass-gradients-main/src/index.jsx
+++ fractal-glass-gradients-main/src/index.jsx
@@ -1,17 +1,11 @@
 import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
-import { Leva, useControls } from "leva";
 import Experience from "./Experience.jsx";
 
 function App() {
-    const { showOverlay } = useControls({
-        showOverlay: true,
-    });
-
     return (
         <>
-            <Leva collapsed />
             <Canvas
                 camera={{
                     fov: 45,
@@ -23,24 +17,7 @@ function App() {
             >
                 <Experience />
             </Canvas>
-
-            {showOverlay && (
-                <div className="overlay">
-                    {/* 👇 NAVBAR REMOVED - ONLY HERO TEXT REMAINS */}
-                    <div className="grid-container bottom-content">
-                        <h1 className="col-span-2">
-                            Fractal
-                            <br />
-                            Glass
-                        </h1>
-                        <p className="col-span-2 subtitle">
-                            Experience the beauty of generative, dynamic
-                            gradients blending seamlessly into glass-like
-                            fractal flutes.
-                        </p>
-                    </div>
-                </div>
-            )}
+            {/* 👇 REMOVED: Overlay, Leva controls, and hero text */}
         </>
     );
 }
